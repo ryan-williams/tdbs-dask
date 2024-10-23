79e4dd667328470cee7bb18daf10bbad297a0660
Ryan Williams <ryan.williams@tiledb.com>
2024-10-23 18:38:12 -0400

    use `tiledb` only (rm `tiledbsoma` version)

--- tests/test_hvg.py
+++ tests/test_hvg.py
@@ -1,18 +1,13 @@
-import pytest
+from tiledbsoma_dask.cli.hvg import load_daskarray, CENSUS_S3, DEFAULT_CENSUS_VERSION, DEFAULT_MEASUREMENT, \
+    DEFAULT_X_LAYER
 
-from tiledbsoma_dask.cli.hvg import load_daskarray, CENSUS_S3
 
-
-@pytest.mark.parametrize("use_tiledbsoma", [True, False])
-def test_census_dask_array_read(use_tiledbsoma):
-    exp_uri = f"{CENSUS_S3}/2024-07-01/soma/census_data/homo_sapiens"
+def test_census_dask_array_read():
+    uri = f"{CENSUS_S3}/{DEFAULT_CENSUS_VERSION}/soma/census_data/homo_sapiens/ms/{DEFAULT_MEASUREMENT}/X/{DEFAULT_X_LAYER}"
     X = load_daskarray(
-        exp_uri=exp_uri,
+        uri=uri,
         chunk_size=1_000,
         nrows=4_000,
-        use_tiledbsoma=use_tiledbsoma,
-        measurement_name="RNA",
-        X_layer_name="raw",
     )
     X = X.compute()
     assert X.shape == (4_000, 60_530)
