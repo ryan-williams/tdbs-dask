59b8a11333d27562d6c9f3d196bf8774e9dbbf26
Ryan Williams <ryan.williams@tiledb.com>
2024-10-23 18:33:57 -0400

    use `tiledbsoma` only (rm `tiledb` version)

--- tests/test_hvg.py
+++ tests/test_hvg.py
@@ -1,16 +1,12 @@
-import pytest
-
 from tiledbsoma_dask.cli.hvg import load_daskarray, CENSUS_S3
 
 
-@pytest.mark.parametrize("use_tiledbsoma", [True, False])
-def test_census_dask_array_read(use_tiledbsoma):
-    exp_uri = f"{CENSUS_S3}/2024-07-01/soma/census_data/homo_sapiens"
+def test_census_dask_array_read():
+    uri = f"{CENSUS_S3}/2024-07-01/soma/census_data/homo_sapiens"
     X = load_daskarray(
-        exp_uri=exp_uri,
+        uri=uri,
         chunk_size=1_000,
         nrows=4_000,
-        use_tiledbsoma=use_tiledbsoma,
         measurement_name="RNA",
         X_layer_name="raw",
     )
--- tiledbsoma_dask/cli/hvg.py
+++ tiledbsoma_dask/cli/hvg.py
@@ -4,7 +4,6 @@ import anndata as ad
 import dask.array as da
 import scanpy as sc
 import tiledbsoma
-import tiledb  # Must come after tiledbsoma! https://github.com/single-cell-data/TileDB-SOMA/issues/2293
 from click import option
 from scipy import sparse
 from somacore import AxisQuery
@@ -33,59 +32,43 @@ def sparse_chunk(
     block_id,
     block_info,
     uri: str,
-    use_tiledbsoma: bool,
     measurement_name: str,
     X_layer_name: str,
 ):
     shape = block_info[None]["chunk-shape"]
     array_location = block_info[None]["array-location"]
     (obs_start, obs_end), (var_start, var_end) = array_location
-    obs_slice = slice(obs_start, obs_end - (1 if use_tiledbsoma else 0))
-    var_slice = slice(var_start, var_end - (1 if use_tiledbsoma else 0))
-    ctx = tiledb.Ctx(CTX)
-    if use_tiledbsoma:
-        with tiledbsoma.open(uri, context=SOMATileDBContext(ctx)) as exp:
-            with exp.axis_query(
-                measurement_name=measurement_name,
-                obs_query=AxisQuery(coords=(obs_slice,)),
-                var_query=AxisQuery(coords=(var_slice,)),
-            ) as query:
-                X = query.X(X_layer_name)
-                tbl = X.tables().concat()
-        soma_dim_0, soma_dim_1, count = [ col.to_numpy() for col in tbl.columns ]
-    else:
-        with tiledb.open(uri, ctx=ctx) as tiledb_array:
-            block = tiledb_array[obs_slice, var_slice]
-        soma_dim_0, soma_dim_1, count = block["soma_dim_0"], block["soma_dim_1"], block["soma_data"]
+    obs_slice = slice(obs_start, obs_end - 1)
+    var_slice = slice(var_start, var_end - 1)
+    with tiledbsoma.open(uri, context=SOMATileDBContext(tiledb_config=CTX)) as exp:
+        with exp.axis_query(
+            measurement_name=measurement_name,
+            obs_query=AxisQuery(coords=(obs_slice,)),
+            var_query=AxisQuery(coords=(var_slice,)),
+        ) as query:
+            X = query.X(X_layer_name)
+            tbl = X.tables().concat()
+    soma_dim_0, soma_dim_1, count = [ col.to_numpy() for col in tbl.columns ]
     soma_dim_0 = soma_dim_0 - obs_start
     soma_dim_1 = soma_dim_1 - var_start
     return sparse.csr_matrix((count, (soma_dim_0, soma_dim_1)), shape=shape)
 
 
 def load_daskarray(
-    exp_uri: str,
+    uri: str,
     chunk_size: int,
     nrows: int,
-    use_tiledbsoma: bool,
     measurement_name: str = DEFAULT_MEASUREMENT,
     X_layer_name: str = DEFAULT_X_LAYER,
 ):
     """Load a TileDB-SOMA X layer as a Dask array, using ``tiledb`` or ``tiledbsoma``."""
-    ctx = tiledb.Ctx(CTX)
-    layer_uri = f"{exp_uri}/ms/{measurement_name}/X/{X_layer_name}"
-    uri = exp_uri if use_tiledbsoma else layer_uri
-    if use_tiledbsoma:
-        soma_ctx = SOMATileDBContext(tiledb_ctx=ctx)
-        with tiledbsoma.open(uri, context=soma_ctx) as exp:
-            X = exp.ms[measurement_name].X
-            layer = X[X_layer_name]
-            _, _, data_dtype = layer.schema.types
-            dtype = data_dtype.to_pandas_dtype()
-            nvars = layer.shape[1]
-    else:
-        with tiledb.open(uri, ctx=ctx) as tiledb_array:
-            dtype = tiledb_array.dtype
-            nvars = tiledb_array.shape[1]
+    soma_ctx = SOMATileDBContext(tiledb_config=CTX)
+    with tiledbsoma.open(uri, context=soma_ctx) as exp:
+        X = exp.ms[measurement_name].X
+        layer = X[X_layer_name]
+        _, _, data_dtype = layer.schema.types
+        dtype = data_dtype.to_pandas_dtype()
+        nvars = layer.shape[1]
 
     X = da.map_blocks(
         sparse_chunk,
@@ -97,7 +80,6 @@ def load_daskarray(
         uri=uri,
         measurement_name=measurement_name,
         X_layer_name=X_layer_name,
-        use_tiledbsoma=use_tiledbsoma,
     )
     return X
 
@@ -107,21 +89,18 @@ def hvg(
     measurement_name: str = DEFAULT_MEASUREMENT,
     mus_musculus: bool = False,
     nrows: int = DEFAULT_NROWS,
-    no_tiledbsoma: bool = False,
     census_version: str = DEFAULT_CENSUS_VERSION,
     X_layer_name: str = DEFAULT_X_LAYER,
 ):
     """Compute highly-variable genes on a subset of CELLxGENE Census data, reading TileDB-SOMA data using Dask."""
     species: Species = "mus_musculus" if mus_musculus else "homo_sapiens"
     soma_uri = f"{CENSUS_S3}/{census_version}/soma"
-    exp_uri = f"{soma_uri}/census_data/{species}"
-    use_tiledbsoma = not no_tiledbsoma
+    uri = f"{soma_uri}/census_data/{species}"
     X = load_daskarray(
-        exp_uri=exp_uri,
+        uri=uri,
         chunk_size=chunk_size,
         measurement_name=measurement_name,
         nrows=nrows,
-        use_tiledbsoma=use_tiledbsoma,
         X_layer_name=X_layer_name,
     )
 
@@ -136,7 +115,6 @@ def hvg(
 @option('-m', '--mus-musculus', is_flag=True, help="Query Census for mouse data (default: human)")
 @option('-M', '--measurement-name', default=DEFAULT_MEASUREMENT, help=f'Experiment "measurement" to read (default: "{DEFAULT_MEASUREMENT}")')
 @option('-n', '--nrows', type=int, default=DEFAULT_NROWS)
-@option('-S', '--no-tiledbsoma', is_flag=True, help='Load `X` array chunks using `tiledb` instead of `tiledbsoma`')
 @option('-v', '--census-version', default=DEFAULT_CENSUS_VERSION, help="")
 @option('-X', '--X-layer-name', 'X_layer_name', default=DEFAULT_X_LAYER, help=f'"X" layer to read (default: "{DEFAULT_X_LAYER}")')
 def hvg_cmd(*args, **kwargs):
